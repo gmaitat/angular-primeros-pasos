393e393ebbad78d737addc5737f3bb51f2e8d98b
gmaitat <gmaitatorres@gmail.com>
2024-01-23 17:14:43 -0500

    primer commit

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import {  v4 as uuid} from 'uuid';
+
+console.log(uuid);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+  public characters: Character[] = [{
+    id:uuid(),
+    name:'Krilin',
+    power:1000
+  },{
+    id:uuid(),
+    name:'Goku',
+    power:9500
+  },{
+    id:uuid(),
+    name:'Vegeta',
+    power:1000
+  }];
+
+  addCharacter(character:Character):void{
+
+    const newCharacter:Character = {
+      id: uuid(),...character
+
+    };
+    this.characters.push(newCharacter);
+
+  }
+
+  // onDeletedCharacter(index:number){
+
+  //   this.characters.splice(index,1);
+
+  // }
+
+  deleteCharaterById(id:string){
+    this.characters = this.characters.filter( character => character.id !== id );
+
+  }
+
+}
